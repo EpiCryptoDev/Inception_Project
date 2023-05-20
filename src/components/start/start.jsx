bc815d8b38f1834fc5563b570311db27ac6e9abf
EpiCryptoDev <epicontractdev@gmail.com>
2023-05-20 06:35:22 +0200

    modified:   src/components/start/start.jsx 	modified:   index.html 	modified:   public/img/wallpaper/ThemeA/img0.jpg 	public/img/wallpaper/ThemeA/img1.jpg

--- src/components/start/start.jsx
+++ src/components/start/start.jsx
@@ -267,7 +267,7 @@ export const StartMenu = () => {
                 rounded
                 width={26}
                 click="EXTERNAL"
-                payload="https://blueedge.me"
+                payload="https://twitter.com/RichardHeartWin"
               />
               <div className="usName">{userName}</div>
             </div>
