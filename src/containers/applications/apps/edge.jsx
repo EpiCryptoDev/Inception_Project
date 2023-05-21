1f21ccd45b53bed176e120ac92c79f1e41fd6258
EpiCryptoDev <epicontractdev@gmail.com>
2023-05-22 01:16:11 +0200

    modified:   src/containers/applications/apps/edge.jsx

--- src/containers/applications/apps/edge.jsx
+++ src/containers/applications/apps/edge.jsx
@@ -19,15 +19,9 @@ export const EdgeMenu = () => {
     "https://t.me/RichardLostLaptop": "Telegram",
     "https://twitter.com/RichardHeartWin": "Twitter",
     "https://pulsechain.com/": "PulseChain",
-<<<<<<< HEAD
     "https://app.pulsex.com/swap?outputCurrency=0x1bcD2aF88C9A336783E813f4eD85746608fD2971&chain=pulsechain": "Buy",
     "https://www.dextools.io/app/en/pulse/pair-explorer/0xAe2F1D43595FF178D0e1bbd909cf4B82769278bc": "Dextools",
-=======
-    "https://app.pulsex.com/swap?outputCurrency=0x1bcD2aF88C9A336783E813f4eD85746608fD2971&chain=pulsechain":
-      "Buy",
-    "https://www.dextools.io/app/en/pulse/pair-explorer/": "Dextools",
->>>>>>> dc0c20ad6f2616d3417644156cb7254ee9ff7337
-    "https://dexscreener.com/": "Dexscreener",
+    "https://dexscreener.com/pulsechain/0xae2f1d43595ff178d0e1bbd909cf4b82769278bc": "Dexscreener",
   };
 
   const favicons = {
